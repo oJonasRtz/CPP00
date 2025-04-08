45f6e72cd1675dd6acde1c832a34d4a937fde191
Jonas <jonasper19@gmail.com>
2025-04-08 06:24:25 -0300

    Megaphone: first version

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 
-int	main(void)
+static void	draw_char(const char c)
 {
-	std::cout << "Hello world\n";
+	std::cout << c;
+}
+
+static int	draw_message(const char *str)
+{
+	if (!str)
+		return (0);
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] == '\\')
+			switch (str[++i])
+			{
+				case 'n': draw_char('\n'); break;
+				case 't': draw_char('\t'); break;
+				case 'b': draw_char('\b'); break;
+				case 'v': draw_char('\v'); break;
+				case 'f': draw_char('\f'); break;
+				case 'a': draw_char('\a'); break;
+				case 'r': draw_char('\r'); break;
+				case '\\': draw_char('\\'); break;
+			}
+		else
+			draw_char((char)std::toupper(str[i]));
+	}
 	return (0);
+}
+
+static int	manage_messages(int argc, char **argv)
+{
+	int	i = 0;
+
+	if (argc == 1)
+		return (draw_message("* LOUD AND UNBEARABLE FEEDBACK NOISE *\n"));
+	while (argv[i])
+		draw_message(argv[i++]);
+	draw_char('\n');
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	return (manage_messages(argc, ++argv));
 }
